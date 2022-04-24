eeff388ae946e1328e87207d6b7f0915c3c99571
TheMisfitF0x <77797280+TheMisfitF0x@users.noreply.github.com>
2022-04-24 15:13:01 -0400

    implement contact
    other things also done, too tired to elaborate

--- model/model.js
+++ model/model.js
@@ -1,6 +1,85 @@
 //All the Content!
-var homeContent = '<div class="home"><div class="head-space"><div class="header-text-wrapper"><p class="header">Header goes here</p><p class="subheader">Less important text here</p><p class="altheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Fugiat aliquid minus nemo sed est.</p><button class="header-button">Read More</button></div></div><div class="quote"><div class="quote-text">“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium totam remaperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo”</div><div class="quote-name">John Smith</div><div class="quote-title">Corporation CEO, books author.</div></div><div class="upcoming-events"><div class="events-header">Upcoming Events:<hr></div><div class="events-wrapper"><div class="event"><div class="date-block"><div class="day">06</div><div class="month">JUN</div></div><div class="event-text"><div class="event-title">Sed et persipiatis</div><div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div></div></div><div class="event"><div class="date-block"><div class="day">30</div><div class="month">JUN</div></div><div class="event-text"><div class="event-title">Sed et persipiatis</div><div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div></div></div><div class="event"><div class="date-block"><div class="day">30</div><div class="month">AUG</div></div><div class="event-text"><div class="event-title">Sed et persipiatis</div><div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div></div></div><div class="event"><div class="date-block"><div class="day">23</div><div class="month">NOV</div></div><div class="event-text"><div class="event-title">Sed et persipiatis</div><div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div></div></div><div class="event"><div class="date-block"><div class="day">23</div><div class="month">DEC</div></div><div class="event-text"><div class="event-title">Sed et persipiatis</div><div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit.</div></div></div></div></div></div>';
-var aboutContent = '<div class="about"><div class="about-header"><div class="header-image"></div><div class="header-text">OUR HISTORY:<hr></div></div><div class="about-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemoenim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliqua quaerat voluptatem.</div ></div > ';
+var homeContent = `<div class="home">
+<div class="head-space">
+  <div class="header-text-wrapper">
+    <p class="header">Header goes here</p>
+    <p class="subheader">Less important text here</p>
+    <p class="altheader">Lorem ipsum dolor sit amet, consectetur adipisicing elit.Fugiat aliquid minus nemo sed
+      est.</p><button class="header-button">Read More</button>
+  </div>
+</div>
+<div class="quote">
+  <div class="quote-text">“Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque
+    laudantium totam remaperiam eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta
+    sunt explicabo. Iste natus error sit voluptatem accusantium doloremque laudantium totam rem aperiam eaque ipsa
+    quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo”</div>
+  <div class="quote-name">John Smith</div>
+  <div class="quote-title">Corporation CEO, books author.</div>
+</div>
+<div class="upcoming-events">
+  <div class="events-header">Upcoming Events:
+    <hr>
+  </div>
+  <div class="events-wrapper">
+    <div class="event">
+      <div class="date-block">
+        <div class="day">06</div>
+        <div class="month">JUN</div>
+      </div>
+      <div class="event-text">
+        <div class="event-title">Sed et persipiatis unde omnis iste natus</div>
+        <div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos
+          aspernatur</div>
+      </div>
+    </div>
+    <div class="event">
+      <div class="date-block">
+        <div class="day">30</div>
+        <div class="month">JUN</div>
+      </div>
+      <div class="event-text">
+        <div class="event-title">Sed et persipiatis unde omnis iste natus</div>
+        <div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos
+          aspernatur</div>
+      </div>
+    </div>
+    <div class="event">
+      <div class="date-block">
+        <div class="day">30</div>
+        <div class="month">AUG</div>
+      </div>
+      <div class="event-text">
+        <div class="event-title">Sed et persipiatis unde omnis iste natus</div>
+        <div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos
+          aspernatur</div>
+      </div>
+    </div>
+    <div class="event">
+      <div class="date-block">
+        <div class="day">23</div>
+        <div class="month">NOV</div>
+      </div>
+      <div class="event-text">
+        <div class="event-title">Sed et persipiatis unde omnis iste natus</div>
+        <div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos
+          aspernatur</div>
+      </div>
+    </div>
+    <div class="event">
+      <div class="date-block">
+        <div class="day">23</div>
+        <div class="month">DEC</div>
+      </div>
+      <div class="event-text">
+        <div class="event-title">Sed et persipiatis unde omnis iste natus</div>
+        <div class="event-desc">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquid temporibus quos
+          aspernatur</div>
+      </div>
+    </div>
+  </div>
+</div>
+</div>`;
+var aboutContent = `<div class="about"><div class="about-header"><div class="header-image"></div><div class="header-text">OUR HISTORY:<hr></div></div><div class="about-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemoenim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed quia non numquam eius modi tempora incidunt ut labore et dolore magnam aliqua quaerat voluptatem.</div ></div > `;
 var galleryContent = `<div class="gallery">
 <div class="gallery-header">
   GALLERY:
@@ -98,6 +177,7 @@ var blogContent = `
   </div>
 </div>
 </div>`;
+var contactContent = ``;
 
 
 //Take a pgName from app.js and SCREEEEEE
